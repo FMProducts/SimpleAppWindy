ba2c4d9352a7aa4caa325a6db060ac8f218ea7c3
Mac Os <fariz.mamedowtm@gmail.com>
2022-05-31 00:01:31 +0500

    implement logic

--- app/src/main/java/fm/coding/windy/MainActivity.kt
+++ app/src/main/java/fm/coding/windy/MainActivity.kt
@@ -3,7 +3,10 @@ package fm.coding.windy
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import fm.coding.windy.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlin.properties.Delegates.notNull
 
 class MainActivity : AppCompatActivity() {
@@ -19,13 +22,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupListener()
+        setupObserver()
     }
 
 
     private fun setupListener() = with(binding){
         runFlowButton.setOnClickListener {
-            // run flow
+            val value = valueInput.text.toString().toIntOrNull() ?: 0 // default value 0
+            viewModel.onFlowButtonClicked(value)
         }
+    }
 
+    private fun setupObserver() = with(viewModel){
+        printFlow.onEach {
+            if (it == null) binding.resultTextView.text = "" // if receive null clear text view
+            else {
+                binding.resultTextView.append("\n") // new line
+                binding.resultTextView.append(it.toString())
+            }
+        }.launchIn(lifecycleScope)
     }
 }
--- app/src/main/java/fm/coding/windy/MainViewModel.kt
+++ app/src/main/java/fm/coding/windy/MainViewModel.kt
@@ -1,10 +1,40 @@
 package fm.coding.windy
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
 
-    fun onFlowButtonClicked(){
+    private val _printFlow = MutableStateFlow<Int?>(null)
+    val printFlow = _printFlow.asStateFlow()
+
+    private val nFlow = MutableSharedFlow<Int>(replay = 1000)
+
+    var tempValue = 0
+
+    init {
+        viewModelScope.launch {
+            nFlow.collect{
+                tempValue += it
+                _printFlow.tryEmit(tempValue)
+            }
+        }
+    }
+
+    fun onFlowButtonClicked(value: Int) = viewModelScope.launch{
+        _printFlow.tryEmit(null)
+        tempValue = 0
+        for(index in 0 until value){
+            val delayInMillis = (index + 1) * 100
+            delay(delayInMillis.toLong())
+            nFlow.tryEmit(index + 1)
+        }
+
 
     }
 
